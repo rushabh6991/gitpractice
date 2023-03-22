5a3560c58b285c65643aa1fe310e2ca7b95423a8
rushabh <rushabh6991@gmail.com>
2023-03-22 08:18:08 +0000

    test with feature-101 hello.java

--- hello.java
+++ hello.java
@@ -6,7 +6,7 @@ Public static void main(String args[])
 System.out.println(“hello from local repository”);
 
 System.out.println(“hello from central repository”);
-Feature-101
+Feature-101-2.0
 }
 }
 
